d2f1c50ff34d910dd6ea86d1759522b594908e68
Nicola <nicola.de.filippis@cern.ch>
2016-03-04 04:57:38 +0100

    Update

--- test/HiggsToZZ_mc_noskim_noMuScleFitCalib_no2015EScaleCalib_EA2015_Regr_13TeV_VectorRootTree.py
+++ test/HiggsToZZ_mc_noskim_noMuScleFitCalib_no2015EScaleCalib_EA2015_Regr_13TeV_VectorRootTree.py
@@ -41,36 +41,30 @@ process.goodOfflinePrimaryVertices = cms.EDFilter("VertexSelector",
                                         )
         
 
-usePAT='false'
-
-# Preselection analysis sequence
-if usePAT == 'true':
-  process.load('HiggsAnalysis/HiggsToZZ4Leptons/hTozzTo4leptonsPreselectionPAT_2e2mu_cff')
-else:
-
-  # Electron ordering in pT
-  process.hTozzTo4leptonsElectronOrdering = cms.EDProducer("HZZ4LeptonsElectronOrdering",
+	 
+# Electron ordering in pT	  
+process.hTozzTo4leptonsElectronOrdering = cms.EDProducer("HZZ4LeptonsElectronOrdering",
                                                            electronCollection = cms.InputTag("gsfElectrons")
                                                            )
 
   
-  #process.load('MuonAnalysis/MuonAssociators/muonCleanerBySegments_cfi')
-  #process.cleanMuonsBySegments.src = cms.InputTag("muons")
+#process.load('MuonAnalysis/MuonAssociators/muonCleanerBySegments_cfi')
+#process.cleanMuonsBySegments.src = cms.InputTag("muons")
 
     
-  process.load('HiggsAnalysis/HiggsToZZ4Leptons/hTozzTo4leptonsPreselection_data_noskim_vector_cff') 
-  process.hTozzTo4leptonsElectronSelector.electronCollection = cms.InputTag("gedGsfElectrons")
-  # process.vetoElectrons.src = cms.InputTag("calibratedElectrons")  
-  process.hTozzTo4leptonsHLTInfo.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
-  process.patTrigger.processName=cms.string("HLT")
-  process.hTozzTo4leptonsRootTreePresel.use2011EA = cms.untracked.bool(False)
-  process.hTozzTo4leptonsRootTreePresel.triggerEvent  = cms.InputTag("hltTriggerSummaryAOD","","HLT")
-  process.hTozzTo4leptonsRootTreePresel.fillPUinfo = True
-  process.hTozzTo4leptonsRootTreePresel.fillHLTinfo = cms.untracked.bool(False)                                           
-  process.hTozzTo4leptonsRootTreePresel.triggerFilter = cms.string('hltL3fL1sMu16Eta2p1L1f0L2f16QL3Filtered40Q')
-  #process.hTozzTo4leptonsRootTreePresel.triggerFilterAsym = cms.vstring('hltDiMuonL3PreFiltered8','hltDiMuonL3p5PreFiltered8')
-  process.hTozzTo4leptonsRootTreePresel.fillMCTruth  = cms.untracked.bool(True)    
-  process.hTozzTo4leptonsRootTreePresel.isVBF  = cms.bool(False)
+process.load('HiggsAnalysis/HiggsToZZ4Leptons/hTozzTo4leptonsPreselection_data_noskim_vector_cff') 
+process.hTozzTo4leptonsElectronSelector.electronCollection = cms.InputTag("gedGsfElectrons")
+# process.vetoElectrons.src = cms.InputTag("calibratedElectrons")  
+process.hTozzTo4leptonsHLTInfo.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
+process.patTrigger.processName=cms.string("HLT")
+process.hTozzTo4leptonsRootTreePresel.use2011EA = cms.untracked.bool(False)
+process.hTozzTo4leptonsRootTreePresel.triggerEvent  = cms.InputTag("hltTriggerSummaryAOD","","HLT")
+process.hTozzTo4leptonsRootTreePresel.fillPUinfo = True
+process.hTozzTo4leptonsRootTreePresel.fillHLTinfo = cms.untracked.bool(False)                                           
+process.hTozzTo4leptonsRootTreePresel.triggerFilter = cms.string('hltL3fL1sMu16Eta2p1L1f0L2f16QL3Filtered40Q')
+#process.hTozzTo4leptonsRootTreePresel.triggerFilterAsym = cms.vstring('hltDiMuonL3PreFiltered8','hltDiMuonL3p5PreFiltered8')
+process.hTozzTo4leptonsRootTreePresel.fillMCTruth  = cms.untracked.bool(True)    
+process.hTozzTo4leptonsRootTreePresel.isVBF  = cms.bool(False)
 
 process.genanalysis= cms.Sequence(
         process.hTozzTo4leptonsGenSequence                  *
@@ -98,15 +92,10 @@ process.hTozzTo4leptonsSelectionPath = cms.Path(
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 readFiles = cms.untracked.vstring(
-#'file:zz.root'
-#'file:pickevents_251562_143_130298882.root'
-#'file:ZZTo4L_13TeV_powheg_pythia8_003264D6-C815-E511-8D7A-00A0D1EE26D0.root'
-#'file:reco_RAW2DIGI_L1Reco_RECO.root'
-#'/store/mc/RunIISpring15DR74/ZZ_TuneCUETP8M1_13TeV-pythia8/AODSIM/Startup25ns_EXOReReco_74X_Spring15_mcRun2_startup25ns_v0-v1/10000/04F5B26D-107B-E511-9BF2-20CF3027A5FD.root'
 #'root://xrootd.ba.infn.it//store/mc/RunIISpring15DR74/ZZ_TuneCUETP8M1_13TeV-pythia8/AODSIM/Startup25ns_EXOReReco_74X_Spring15_mcRun2_startup25ns_v0-v1/10000/04F5B26D-107B-E511-9BF2-20CF3027A5FD.root'
 'file:/lustre/cms/store/user/defilip/MonoHiggs/jobsreco_MZP1200_MA0300/reco_MZP1200_MA0300_2501.root'
 )
