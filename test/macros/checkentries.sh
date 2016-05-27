f8a7eb2ba77bbfc4c2b86a93ae4a12d3330d8d70
Nicola <nicola.de.filippis@cern.ch>
2016-05-27 17:46:49 +0200

    Fix

--- test/macros/checkentries.sh
+++ test/macros/checkentries.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# for file in `ls  /lustre/cms/store/user/defilip/Summer12_53X_paper_step_analysis_merged/roottree_leptons_QCD*_*8TeV*.root`; do
-# for file in `ls  /lustre/cms/store/user/defilip/Fall11_445_paper_step_analysis_merged/roottree_leptons_GluGluToH*7TeV*.root`; do
-# for file in `ls  /lustre/cms/store/user/defilip/Fall11_445_paper_step_analysis_merged_local/roottree_leptons_*.root`; do
-# for file in `cat filelist_4mu_2012_paper_Bari.txt`; do
-# for file in `ls -lstrd1 /lustre/cms/store/user/defilip/histos2e2mu/*.root | grep -v bnn | awk '{print $10}'`; do
-for file in `ls  /lustre/cms/store/user/defilip/Summer12_53X_paper_step_analysis_merged_local/roottree_leptons_Summer12_53X_step1_paper_VBFToHToZZTo4L_M-600*`; do
+for file in `ls /lustre/cms/store/user/dburns/MonoHiggs/Fall15_25ns_merged/*WJet*`; do
 echo $file
 cat checkentries.C | sed "s?filename?${file}?g" > tmp.C
 g++ -I $ROOTSYS/include tmp.C `root-config --glibs` `root-config --libs` `root-config --cflags` -o checkentries
